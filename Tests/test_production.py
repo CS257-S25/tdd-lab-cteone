28e8127abbc60b1047937e5c3450cf0fe45505ba
cteone <108945107+cteone@users.noreply.github.com>
2025-04-11 15:12:10 -0500

    Added Tests

--- Tests/test_production.py
+++ Tests/test_production.py
@@ -1,3 +1,25 @@
-'''
-A file for tests for the production code.
-'''
+import unittest
+
+from production import reverse_word, reverse_all_words
+
+
+class TestReverseWord(unittest.TestCase):
+    def test_reverse(self):
+        self.assertEqual(reverse_word("hello"), "olleh", "Reverse word")
+
+    def test_reverse_empty(self):
+        self.assertEqual(reverse_word(""), "", "Reverse empty string")
+
+
+class TestReverseAllWords(unittest.TestCase):
+    def test_reverse(self):
+        self.assertEqual(
+            reverse_all_words("this is a test"), "siht si a tset", "Reverse sentence"
+        )
+
+    def test_reverse_empty(self):
+        self.assertEqual(reverse_all_words(""), "", "Should be empty")
+
+
+if __name__ == "__main__":
+    unittest.main()
--- production.py
+++ production.py
@@ -1,3 +1,22 @@
-'''
-A file for the production code
-'''
+import sys
+
+
+def reverse_word(word):
+    new_word = ""
+    for letter in word:
+        new_word = letter + new_word
+    return new_word
+
+
+def reverse_all_words(phrase):
+    words = phrase.split(" ")
+    new_words = map(lambda word: reverse_word(word), words)
+    return " ".join(new_words)
+
+
+def main():
+    try:
+        phrase = sys.argv[1]
+        print(reverse_all_words(phrase))
+    except IndexError:
+        print("Specify phrase")
